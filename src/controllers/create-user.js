5cebdda3c7ab8c7bc717f307f15d0f08abd2eaec
AlanFMA <alanmonteiro105@gmail.com>
2025-02-26 12:01:19 -0300

    feat: add CreateUseController and validations

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,11 +1,12 @@
 import { CreateUserUseCase } from '../use-cases/create-user.js';
+import validator from 'validator';
+import { badRequest, created, serverError } from './helpers.js';
 
 export class CreateUserUseController {
     async execute(httpRequest) {
         try {
             const params = httpRequest.body;
 
-            // validar a requisição (campos obrigatórios, tamanho de senha e email)
             const requiredFields = [
                 'first_name',
                 'last_name',
@@ -15,32 +16,34 @@ export class CreateUserUseController {
 
             for (const field of requiredFields) {
                 if (!params[field] || params[field].trim().length === 0) {
-                    return {
-                        statusCode: 400,
-                        body: {
-                            message: `Missing param: ${field}`,
-                        },
-                    };
+                    return badRequest({ message: `Missing param: ${field}` });
                 }
             }
-            // chamar o use case
-            // retornar a resposta para o usuário (status code)
+
+            const passwordIsInvalid = params.password.length < 6;
+
+            if (passwordIsInvalid) {
+                return badRequest({
+                    message: 'Password must be at least 6 characters',
+                });
+            }
+
+            const emailIsValid = validator.isEmail(params.email);
+
+            if (!emailIsValid) {
+                return badRequest({
+                    message: 'Invalid e-mail. Please provide a valid one.',
+                });
+            }
+
             const createUserUseCase = new CreateUserUseCase();
 
             const createdUser = await createUserUseCase.execute(params);
 
-            return {
-                statusCode: 201,
-                body: createdUser,
-            };
+            return created(createdUser);
         } catch (error) {
             console.error(error);
-            return {
-                statusCode: 500,
-                body: {
-                    errorMessage: 'internal server error',
-                },
-            };
+            return serverError();
         }
     }
 }
