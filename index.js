5e3fcddbf83136e60d74f594c858f6064794019b
AlanFMA <alanmonteiro105@gmail.com>
2025-02-27 16:40:33 -0300

    feat: add GetUserByIdController and validations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config.js';
 import express from 'express';
 import { CreateUserUseController } from './src/controllers/create-user.js';
+import { GetUserByIdController } from './src/controllers/get-user-by-id.js';
 
 const app = express();
 
@@ -10,6 +11,14 @@ app.post('/api/users', async (request, response) => {
     const CreateUserController = new CreateUserUseController();
 
     const { statusCode, body } = await CreateUserController.execute(request);
+
+    response.status(statusCode).json(body);
+});
+
+app.get('/api/users/:userId', async (request, response) => {
+    const getUserByIdController = new GetUserByIdController();
+
+    const { statusCode, body } = await getUserByIdController.execute(request);
 
     response.status(statusCode).json(body);
 });
--- src/repositories/postgres/get-user-by-id.js
+++ src/repositories/postgres/get-user-by-id.js
@@ -1,7 +1,7 @@
 import { PostgresHelper } from '../../db/postgres/helper.js';
 
 export class PostgressGetUserByIdRepository {
-    async execut(userId) {
+    async execute(userId) {
         const user = await PostgresHelper.query(
             'SELECT * FROM users WHERE id = $1',
             [userId],
