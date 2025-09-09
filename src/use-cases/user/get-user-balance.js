f4ed4bf963bff4ca9aad23d4d37dd6e793d92c8b
alanFMA <alanmonteiro105@gmail.com>
2025-09-09 10:13:56 -0300

    feat: add GetUserBalanceUseCase

---
+++ src/use-cases/user/get-user-balance.js
@@ -0,0 +1,19 @@
+export class GetUserBalanceUseCase {
+    constructor({ getUserBalanceRepository, getUserByIdRepository }) {
+        this.getUserBalanceRepository = getUserBalanceRepository;
+        this.getUserByIdRepository = getUserByIdRepository;
+    }
+    async execute(params) {
+        const { userId } = params;
+
+        const user = await this.getUserByIdRepository.execute(userId);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const balance = await this.getUserBalanceRepository.execute(userId);
+
+        return balance;
+    }
+}
