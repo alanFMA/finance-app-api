84449b8d07bb86a6f83c853255179575bd73e3e4
alanFMA <alanmonteiro105@gmail.com>
2025-09-10 11:26:38 -0300

    feat: ensure CreateTransaction and DeleteTransaction repositories use Prisma

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,12 +1,16 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../database/prisma-client.js';
 
 export class PostgresDeleteTransactionRepository {
     async execute(transactionId) {
-        const transaction = await PostgresHelper.query(
-            'DELETE FROM transactions WHERE id = $1 RETURNING *',
-            [transactionId],
-        );
-
-        return transaction[0];
+        try {
+            return await prisma.transactions.delete({
+                where: {
+                    id: transactionId,
+                },
+            });
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
     }
 }
