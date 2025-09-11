5ea1b8f881d98a832b5fc575acaf40e4f249be5e
alanFMA <alanmonteiro105@gmail.com>
2025-09-11 10:48:10 -0300

    feat: add email and user error handling

--- src/errors/user.ts
+++ src/errors/user.ts
@@ -1,12 +1,12 @@
 export class EmailAlreadyInUseError extends Error {
-    constructor(email) {
+    constructor(email: string) {
         super(`This email '${email}' is already in use.`);
         this.name = 'EmailAlreadyInUseError';
     }
 }
 
 export class UserNotFoundError extends Error {
-    constructor(userId) {
+    constructor(userId: string) {
         super(`User with id ${userId} not found.`);
         this.name = 'UserNotFoundError';
     }
